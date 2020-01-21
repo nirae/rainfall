97294045687f8ab27bca6abcf7133459c2756298
nirae <hello@nicolasdubouilh.fr>
2020-01-21 19:09:38 +0100

    level7 ok | starting level8

---
+++ level7/source.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+char c[50]; /* size ??? */
+
+void m(void) {
+  time_t t;
+
+  t = time(0);
+  printf("%s - %d\n", c, (int)t);
+  return;
+}
+
+int main(int ac, char **av) {
+  char *str_1;
+  char *str_2;
+  FILE *pass;
+
+  str_1 = (char *)malloc(8);
+  str_1[0] = 1;
+  str_1[4] = malloc(8);
+
+  str_2 = (char *)malloc(8);
+  str_2[0] = 2;
+  str_2[4] = malloc(8);
+
+  strcpy(str_1[4], av[1]);
+  strcpy(str_2[4], av[2]);
+
+  pass = fopen("/home/user/level8/.pass","r");
+  fgets(c, 68, pass);
+  puts("~~");
+  return 0;
+}
